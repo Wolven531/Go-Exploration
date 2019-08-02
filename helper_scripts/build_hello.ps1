6d88cd22270ee05b681b08d47cef63e7111e937c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-01 22:32:50 -0600

    Add further instructions to build script

--- helper_scripts/build_hello.ps1
+++ helper_scripts/build_hello.ps1
@@ -1,2 +1,7 @@
+# NOTE: MUST be run from working directory `helper_scripts`
+
 Set-Location -Path '../src/hello/'
 Invoke-Expression -Command 'go build'
+
+Write-Host -ForegroundColor Green 'You can now run `hello.exe` from this location'
+Write-Host -ForegroundColor Yellow '(or `cd ..\..\helper_scripts` to return)'
