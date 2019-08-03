cd4c0677854cbff20540ceab96ad0b7532b3e80c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-08-02 22:08:27 -0600

    Use multiple variable declaration

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,15 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// NOTE: go-lint complains type can be inferred for below line
-	// var greeting string = "Hello, world!"
+	// // NOTE: go-lint complains type can be inferred for below line
+	// // var greeting string = "Hello, world!"
 
-	// NOTE: can declare and assign using `:=` w/o var keyword
-	greeting := "Hello, "
-	// NOTE: can declare and assign using `var` and `=`
-	var greeting2 = ", friend!"
-	// NOTE: strings are denoted w/ double quote (") OR backtick (`)
-	name := `Anthony`
+	// // NOTE: can declare and assign using `:=` w/o var keyword
+	// greeting := "Hello, "
+	// // NOTE: can declare and assign using `var` and `=`
+	// var greeting2 = ", friend!"
+	// // NOTE: strings are denoted w/ double quote (") OR backtick (`)
+	// name := `Anthony`
+
+	// NOTE: multiple variable declaration; no commas, just one line per declaration
+	var (
+		greeting  = "Hello, "
+		greeting2 = ", friend!"
+		name      = `Anthony`
+	)
 
 	// fmt.Printf(greeting + greeting2 + "\n")
 	fmt.Println(greeting + name + greeting2)
